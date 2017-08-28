0a93749650c48965413426f503b484ee75f839d8
Joecracko <joecracko@gmail.com>
2017-08-28 07:44:31 -0400

    Adding content to the About page.

--- ClientApp/components/pages/About.tsx
+++ ClientApp/components/pages/About.tsx
@@ -17,10 +17,25 @@ export class About extends React.Component<{}, {}> {
                         <div className="col-xs-12">
                             <HeroText>
                                 <p>
-                                    Ascend Parkour is committed to providing its students with a safe, engaging,
-                                    and memorable learning experience. Our mission is to help our students realize
-                                    their potential physically, mentally, emotionally, and interpersonally through
-                                    engaging personal development.
+                                    We are committed to providing its students with a safe, engaging, and memorable 
+                                    learning experience. Our mission is to help our students realize their potential 
+                                    physically, mentally, emotionally, and interpersonally through engaging personal 
+                                    development.
+                                </p>
+                                <p>
+                                    Ascend Parkour is an aspiring fitness community in the Scranton area.  Parkour 
+                                    focuses on using creativity and everyday surroundings to find fitness 
+                                    opportunities anywhere.  Therefore, many classes will be held in outdoor venues 
+                                    such as parks and urban settings.  Being in these "real-life" environments allows 
+                                    students to develop their perspective to view their entire world as an opportunity 
+                                    for growth.
+                                </p>
+                                <p>
+                                    Our head coach is John George.  John became fascinated with parkour as a young 
+                                    child and has been practicing for over 10 years.  In 2011, John became a certified 
+                                    parkour trainer with ADAPT Qualifications.  John is thrilled and eager to establish 
+                                    a parkour community in the Scranton area and help others discover the joys and 
+                                    excitement of an alternative fitness model.
                                 </p>
                             </HeroText>
                         </div>
